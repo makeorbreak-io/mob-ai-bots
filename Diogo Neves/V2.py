d3f4e31a2e0812e2fc5f2f104e9424ece33ee543
Diogo Neves <>
2018-03-15 16:35:27 +0000

    Update bot

---
+++ Diogo Neves/V2.py
@@ -0,0 +1,31 @@
+import random
+import json
+import sys
+
+import multipaint
+
+
+class MegaBot4000(object):
+
+    move_number = 1
+    move_type = "walk"
+    direction = [1, 0]
+
+    def __init__(self, player_id):
+        self.player_id = player_id
+
+    def next_move(self, state):
+        if move_number % 2 == 0:
+            move_type = "walk"
+            direction = random.choice([[1, 0], [-1, 0], [0, 1], [0, -1]])
+        else:
+            move_type = "shot"
+
+        return {
+            "type": move_type,
+            "direction": direction,
+        }
+
+
+if __name__ == "__main__":
+    multipaint.run(MegaBot4000)
